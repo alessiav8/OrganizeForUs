24a7293abc460b42c3a4aa8950fc959766053264
ema5584 <mole_emanuel@yahoo.com>
2022-06-01 17:55:25 +0200

    Revert "Revert "+Avatar""
    This reverts commit 9c880475dd5610a16e9caf68536b69870a9c7a94.

--- organizeforus/app/helpers/application_helper.rb
+++ organizeforus/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    
+    def user_avatar(user, size=50)
+
+        if user.avatar.representable?
+            user.avatar.representation(resize_to_limit: [size, size])
+        else
+            "pikachu.png"
+        end
+    end
 end
