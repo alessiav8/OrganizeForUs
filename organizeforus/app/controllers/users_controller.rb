694ede599ac8509e09657a52c14ae5ae321c597f
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2022-09-11 20:10:14 +0200

    bug calendario

--- organizeforus/app/controllers/users_controller.rb
+++ organizeforus/app/controllers/users_controller.rb
@@ -7,9 +7,14 @@ class UsersController < ApplicationController
     @user= User.find(current_user.id)
     @groups=Group.where(user_id: @current_user)
     if current_user.access_token?
-      dataInizio=parse_datetime(Date.today.to_s, '00:00').to_datetime
-      dataFine=parse_datetime( (Date.today + 30).to_s, '00:00').to_datetime
-      @event_list =  get_all_events_in_range(current_user, dataInizio, dataFine)
+      dataI=(Date.today - 31 ).to_s
+      dataF=(Date.today).to_s
+      hI="08:00:00"
+      hF="18:00:00"
+      datetimeI = parse_datetime(dataI , hI).to_datetime
+      datetimeF = parse_datetime(dataF , hF).to_datetime
+      @event_list = get_all_events_in_range(current_user , datetimeF , datetimeI)
+      puts @event_list
       @event_list=@event_list.items
     end
 
