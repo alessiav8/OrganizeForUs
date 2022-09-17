32f3e27fac3e917bc958b78119f33855b7cd19e6
ema5584 <mole_emanuel@yahoo.com>
2022-09-17 17:14:07 +0200

    Update groups_controller.rb

--- organizeforus/app/controllers/groups_controller.rb
+++ organizeforus/app/controllers/groups_controller.rb
@@ -264,8 +264,6 @@ include Search
   end
 
   def set_github_repo
-    @group = Group.find(params[:id])
-    url = "https://api.github.com/user/repos"
     token = current_user.gh_access_token
 
     if !token
@@ -273,50 +271,6 @@ include Search
       redirect_to :back
     end
 
-    data = {
-      body: {
-        name: @group.name,
-        description: "Created by an amazing website: OrganizeForUs!!",
-        private: true,
-        client_id: Rails.application.credentials.dig(:github, :github_client_id),
-        client_secret: Rails.application.credentials.dig(:github, :github_client_secret)
-      },
-      headers: {
-        "accept" => "application/vnd.github+json",
-        "auth" => "Bearer "+token
-      } 
-    }.to_j
-    byebug
-    response = HTTParty.post(url, data)
-    if response.code == 201
-      token = response.parsed_response['access_token']
-      date = DateTime.now + response.parsed_response['expires_in'].seconds
-      user.update!(access_token: token, expires_at: date)
-    else
-      flash[:error] = "Your token has been expired and we can't refresh it... Please login again with google."
-      redirect_to :back
-    end
-  end
-
-
-  url = URI("https://api.github.com/user/repos")
-  https = Net::HTTP.new(url.host, url.port)
-  https.use_ssl = true
-  request = Net::HTTP::Post.new(url)
-  request["Authorization"] = "token #{token}"
-  request["Content-Type"] = 'application/json'
-  request["Accept"] = 'application/vnd.github+json'
-  request.body = {name: "palla", private: true}.to_json
-  response = https.request(request)
-  json = JSON.parse(response.body, symbolize_names: true)
-  
-  url = json[:html_url]
-
-  json[:owner][:repos_url]
-  r = HTTParty.get(json[:owner][:repos_url])
-  url_repo = r.parsed_response[0]["html_url"]
-
-  def set_github_repo
     @group=Group.find(params[:id])
 
     url = URI("https://api.github.com/user/repos")
